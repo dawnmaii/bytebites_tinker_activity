004ca1a5014229f323372fa0f0ac68adb81dadb5
Dawn Mai <dawnmaii@outlook.com>
2026-06-20 21:23:44 -0700

    added empty models.py with class list

---
+++ models.py
@@ -0,0 +1,7 @@
+# Item class: have name, price, category, popularity rating
+
+# Catalog class: list all items, filter option
+
+# Customer class: track customer name, verification, purchase history
+
+# Transaction class: calculate total cost, add items to order
